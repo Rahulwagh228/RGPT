bc8be2ca031051174ff8349334e0177f826779c6
Rahulwagh228 <127666835+Rahulwagh228@users.noreply.github.com>
2023-10-06 20:33:29 +0530

    first commit

---
+++ Component/Sidebar.tsx
@@ -0,0 +1,35 @@
+
+"use client"
+import { CiCircleChevLeft } from "react-icons/ci";
+import React, { useState } from 'react';
+import Image from "next/image";
+import Logo from "@/app/assets/Images/Asset1.png";
+
+export default function Sidebar() {
+  const [Open, setOpen] = useState(true);
+  const Menus = [
+    {title : "Dashboard", src: ""}
+  ]
+
+  return (
+    <div className="flex">
+      <div className={` ${Open ? " w-72" : " w-20"} p-4 pt-8 duration-300 h-screen bg-dark-purple relative`}>
+
+        <CiCircleChevLeft className={`absolute cursor-pointer -right-4 top-9  h-8 w-8 rounded-full bg-white
+         ${!Open && "rotate-180"} `}
+          onClick={() => setOpen(!Open)}
+        />
+
+        <div className="flex gap-x-4 items-center ">
+          <Image src={Logo} alt="logo" className={` ${Open ? "h-16 w-20" : "h-9 w-10"} rounded-full cursor-pointer duration-200 `}/>
+          <h1 className={`text-white font-medium origin-left text-xl duration-200 ${!Open && "scale-0"}`}>Rgpt</h1>
+        </div>
+      </div>
+
+
+      <div className="p-7 text-2xl font-semibold flex-1 h-screen">
+        <h1>Home Page</h1>
+      </div>
+    </div>
+  )
+}
